6435b28266c4cc47792f7435aaaac12c9debe99f
GioXdaZ <gio.daz@gmail.com>
2022-06-11 12:07:59 -0400

    Commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,40 @@ import "./pokedexStyles/pokedexStyles.css";
 import { PokedexTop } from "./components/PokedexTop";
 import { PokedexScreen } from "./components/PokedexScreen";
 import { PokedexHinge } from "./components/PokedexHinge";
+import { PokedexCap } from "./components/PokedexCap";
 
 function App() {
   return (
-    <div className="pokeBody">
-      <PokedexTop />
-      <div className="bodyScreen">
-        <PokedexScreen />
-        <div className="pokeBottom">
-          <div className="ligthContainerLarge">
-            <div className="lightRedLarge"></div>
-            <div className="lightGreenLage"></div>
-          </div>
-          <div className="stickLeft"></div>
-          <div className="lightContainerTiny">
-            <div className="lightRedTiny"></div>
-            <div className="lightGreenTiny"></div>
-          </div>
-          <div className="pokePanelInfo">
-            <div className="screenPanel"></div>
-          </div>
-          <div className="arrowPanel"></div>
-          <div className="lightsBottomContainer">
-            <div className="lightGreenBottom"></div>
-            <div className="lightYellowBottom"></div>
-            <div className="lightYellowBottom"></div>
-            <div className="lightYellowBottom"></div>
+    <div className="pokedex">
+      <div className="pokeBody">
+        <PokedexTop />
+        <div className="bodyScreen">
+          <PokedexScreen />
+          <div className="pokeBottom">
+            <div className="ligthContainerLarge">
+              <div className="lightRedLarge"></div>
+              <div className="lightGreenLage"></div>
+            </div>
+            <div className="stickLeft"></div>
+            <div className="lightContainerTiny">
+              <div className="lightRedTiny"></div>
+              <div className="lightGreenTiny"></div>
+            </div>
+            <div className="pokePanelInfo">
+              <div className="screenPanel"></div>
+            </div>
+            <div className="arrowPanel"></div>
+            <div className="lightsBottomContainer">
+              <div className="lightGreenBottom"></div>
+              <div className="lightYellowBottom"></div>
+              <div className="lightYellowBottom"></div>
+              <div className="lightYellowBottom"></div>
+            </div>
           </div>
         </div>
       </div>
-      <PokedexHinge/>
+      <PokedexHinge />
+      <PokedexCap />
     </div>
   );
 }
---
+++ src/components/PokedexCap.jsx
@@ -0,0 +1,31 @@
+import "./PokedexCap.css";
+
+export const PokedexCap = () => {
+  return (
+    <div className="pokeCapBodyBorder">
+      <div className="pokeCapBody">
+        <div className="pokeCapBodyIn">
+          <div className="pokeCapBorderScreen">
+            <div className="pokeCapScreen"></div>
+          </div>
+        </div>
+        <div className="buttonsPanel">
+          <div className="pokeButtonPanelTop">
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+          </div>
+          <div className="pokeButtonPanelBottom">
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+            <div className="pokeButtonPanel"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
